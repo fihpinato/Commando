1b728e120db117671bf93b8036ae518e92e67cf9
fihpinato <fpinatoaguiar@gmail.com>
2017-03-12 22:34:04 -0300

    teste de envio de projetos unity #1

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public GameObject personagem; 		//Recebe a malha 3d do personagem
+	public float velocidade = 5f;
+	public float forcaPulo = 5f;
+	public float gravidade = 9.8f;
+
+	Vector3 moverDirecao = Vector3.zero;
+
+	//Interface do componente de animação
+	Animation anim;
+
+	void Start () {
+
+		anim = personagem.GetComponent<Animation>();
+
+		anim.CrossFade ("idle");
+
+	}
+	
+	void Update () {
+		Mover ();	
+		ControlaAnim ();
+		ControlaRotacao ();
+	}
+
+	void Mover(){
+		CharacterController controller = GetComponent<CharacterController>();
+
+		if(controller.isGrounded){
+
+			moverDirecao = new Vector3 (Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
+
+			moverDirecao = transform.TransformDirection (moverDirecao) * velocidade;
+
+		}
+
+		/*
+		if (Input.GetButtonDown ("Jump")) {
+			moverDirecao.y += forcaPulo;
+		}
+		*/
+
+		moverDirecao.y -= gravidade * Time.deltaTime;
+
+		controller.Move (moverDirecao * Time.deltaTime);
+	}
+
+	void ControlaAnim(){
+		if (Input.GetAxisRaw ("Horizontal") != 0f) {
+			anim.CrossFade ("run");
+		} else {
+			anim.CrossFade ("idle");
+		}
+	}
+
+	void ControlaRotacao(){
+		if (Input.GetAxisRaw ("Horizontal") > 0f) {
+			personagem.transform.rotation = Quaternion.Euler (0f, 90f, 0f);
+		} else if(Input.GetAxisRaw ("Horizontal") < 0f){
+			personagem.transform.rotation = Quaternion.Euler (0f, 270f, 0f);
+		}
+	}
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour {
+
+
+	public float velocidade = 30f;
+	public float tempoDestruir = 5f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Destruir());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * velocidade * Time.deltaTime);
+	}
+
+	IEnumerator Destruir(){
+		yield return new WaitForSeconds (tempoDestruir);
+		Destroy (gameObject);
+	}
+
+	void OnCollisionEnter(Collision c){
+		Destroy (gameObject);
+	}
+}
